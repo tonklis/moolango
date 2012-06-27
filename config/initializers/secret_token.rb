81bdcd8e08371cf6aa70e816ab5a9248d3ab0b94
tonklis <benjamin.hernandez@in2teck.com>
2012-06-27 18:35:04 -0400

    Moolango starts, ruby 3.1.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moolango::Application.config.secret_token = 'facd96867c0d711623e26f60cb61e1d0d3fe5578cd74ee2d7ddde2885251b7f8d2ce4e58d592737f2c9d83651d5a784f7dcdf153d0b09684d3d15f474497408e'
