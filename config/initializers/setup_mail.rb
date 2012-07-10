cbb55153ecee523d15ff85bf261703f93ef0af11
tonklis <benjamin.hernandez@in2teck.com>
2012-07-10 16:55:01 -0400

    Added different email account settings

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -3,8 +3,8 @@ ActionMailer::Base.smtp_settings = {
 	:address	=> "smtp.gmail.com",
 	:port		=> 587,
 	:domain		=> "gmail.com",
-	:user_name	=> "tonklis@gmail.com",
-	:password	=> "C%HM2=F.",
+	:user_name	=> "tonklis2@gmail.com",
+	:password	=> "garywillisbass2",
 	:authentication	=> "plain",
 	:enable_starttls_auto	=> true
 }
