90cee82fea741bfd7872191678abff196c28b563
tonklis <benjamin.hernandez@in2teck.com>
2012-07-18 00:22:26 -0400

    Added buttons and signin and singup actions

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,8 @@ Moolango::Application.routes.draw do
 	
 	devise_scope :user do
   	get "logout", :to => "devise/sessions#destroy"
+		get "signin", :to => "devise/sessions#new"
+		get "signup", :to => "devise/registrations#new"
 	end
 
 	match 'interaction' => 'interaction#create', :as => :interaction
