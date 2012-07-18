81531df64152102323584330b985855bbaee45cb
tonklis <benjamin.hernandez@in2teck.com>
2012-07-17 23:36:54 -0400

    Added landing page

--- config/routes.rb
+++ config/routes.rb
@@ -82,7 +82,7 @@ Moolango::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'display#rooms_listing'
+  root :to => 'display#index'
 
   # See how all your routes lay out with "rake routes"
 
