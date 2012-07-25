cb4fa58f6c9f48dd487172c6b0858a131960e678
tonklis <benjamin.hernandez@in2teck.com>
2012-07-24 21:42:47 -0400

    Added another async approach to the joining room process

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,9 +5,30 @@ class MessagesController < ApplicationController
   end
 
   def create
-		session = params[:message][:session]
-		Pusher[session[6..11]].trigger(session[0..5], {:message => params[:message][:content], :origin => params[:message][:origin]})
+		m_session = params[:message][:session]
+		Pusher[m_session[6..11]].trigger(m_session[0..5], {:message => params[:message][:content], :origin => params[:message][:origin]})
     #@message = Message.create!(params[:message])
   end
+
+	def async_outbound
+
+		TestMailer.new_conversation(params[:topic_id], params[:session_id]).deliver
+	
+		@twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
+		if (Language.find_by_name("english").id == session[:language].to_i)
+	  	number_to_send_to = "6145968264"
+			message = "There's someone waiting on Moolango to talk about #{Topic.find(params[:topic_id]).name}"
+		else 
+			number_to_send_to = "6142826401"
+			message = "Hay una persona en Moolango esperando para hablar de #{Topic.find(params[:topic_id]).name}"
+		end
+		
+		@twilio_client.account.sms.messages.create(
+		  :from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
+		  :to => number_to_send_to,
+		  :body => message
+		)
+		
+	end 
 
 end
