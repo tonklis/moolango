6e42784508bde6be7e4b8cb6056a738246705a95
tonklis <benjamin.hernandez@in2teck.com>
2012-07-31 17:39:06 -0400

    Added google tracking code + better visualization for users that are logging in

--- app/assets/javascripts/googleTracking.js
+++ app/assets/javascripts/googleTracking.js
@@ -1,5 +1,3 @@
-<script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-33131126-1']);
   _gaq.push(['_setDomainName', 'moolango.com']);
@@ -10,5 +8,3 @@
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
   })();
-
-</script>
