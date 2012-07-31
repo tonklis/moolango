ae7bac3d7114c2690a85cfddf964f63a0648e295
tonklis <benjamin.hernandez@in2teck.com>
2012-07-31 16:36:01 -0400

    Added language indication in e-mail

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -1,10 +1,11 @@
 class TestMailer < ActionMailer::Base
   default from: "tonklis@gmail.com"
 
-	def new_conversation(topic, token, user)
+	def new_conversation(topic, token, user, language)
 		@topic = Topic.find(topic)
 		@token = token
 		@user = User.find(user)
+		@language = Language.find(language.to_i)
 		mail(:to => "williambabeaux@gmail.com,tonklis@gmail.com,dmiramon@gmail.com", :subject => "New conversation with #{@user.firstname}, about #{@topic.name}")
   end
 	
