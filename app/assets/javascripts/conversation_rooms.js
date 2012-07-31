a91f0ce476a9a6b0c36962fdbee695abfddd6fd7
Diego <dmiramon@gmail.com>
2012-07-31 19:31:46 -0400

    Conversation room topbar Reconnect fix
    Diego

--- app/assets/javascripts/conversation_rooms.js
+++ app/assets/javascripts/conversation_rooms.js
@@ -1,10 +1,13 @@
 var is_reconnect = false;
 var is_end_call = false;
 var is_audio_only = true;
+var is_ready_for_conversation = false;
+var is_timer_running = false;
+var num_connections = 0;
 var total_time = 600;
 var time_elapsed = 0;
 
-function slidePusher(pusherKey, session, topic_name, topic_hints_size){
+function createPusher(pusherKey, session, topic_name, topic_hints_size){
 	
     var pusher = new Pusher(pusherKey);
     var channel_chat = pusher.subscribe(session.substr(6,6));
@@ -24,14 +27,19 @@ function slidePusher(pusherKey, session, topic_name, topic_hints_size){
 		}
 	});
 
-    var channel_slide = pusher.subscribe(session.substr(18,6));
-	
-		channel_slide.bind(session.substr(12,6), function(data) {
-			$("#current_slide")[0].value = data.current_image;
-			$('#slideImageDiv').css("background-image", "url(/assets/topics/" + topic_name + "/" + (parseInt(data.current_image) + 1) +".jpg)");
-			$("#hint_text")[0].innerHTML = data.current_text;	
-		});
+	var channel_slider = pusher.subscribe(session.substr(18,6));
 
+	channel_slider.bind(session.substr(12,6), function(data) {
+		$("#current_slide")[0].value = data.current_image;
+		$('#slideImageDiv').css("background-image", "url(/assets/topics/" + topic_name + "/" + (parseInt(data.current_image) + 1) +".jpg)");
+		$("#hint_text")[0].innerHTML = data.current_text;	
+	});
+	
+	var channel_messages = pusher.subscribe('channel_messages');
+	
+	channel_messages.bind('end_call', function(data) {
+		endCall();
+	});
 }
 
 function nextInteraction(event){
@@ -39,6 +47,7 @@ function nextInteraction(event){
 }
 
 function connect() {
+	session.addEventListener('connectionCreated', connectionCreatedHandler);
 	session.addEventListener('sessionConnected', sessionConnectedHandler);
 	session.addEventListener('streamCreated', streamCreatedHandler); 
 	session.addEventListener('sessionDisconnected', sessionDisconnectedHandler); 
@@ -48,29 +57,31 @@ function connect() {
 }
 
 function reconnect() {
-	is_reconnect = true;
 	session.signal();
-	//setTimeout(function(){connect();}, 1000);
 }
 
 function endCall() {
 	is_end_call = true;
-	session.signal();
+	document.getElementById("videoBtn").style.display = 'none';
+	is_timer_running = false;
+	session.disconnect();
+}
+
+function connectionCreatedHandler(event) {
+	num_connections += event.connections.length;
 }
 
 function sessionConnectedHandler(event) {
 	session.publish(publisher);
-	
 	if ($('#videoBtn').length == 0) {
-		var parentDiv = document.getElementById("publisherDiv");
 		var botonDiv = document.createElement('div');
-		botonDiv.innerHTML = '<input type="button" id="videoBtn" value="Turn on video" onClick="enableDisableVideo()" style="display:none;"/>';
+		botonDiv.innerHTML = '<input type="button" id="videoBtn" class="conversationButton" value="Turn on video" onClick="enableDisableVideo()" style="display:none;"/>';
 		botonDiv.style.position = 'absolute';
 		botonDiv.style.left = '80px';
 		botonDiv.style.top =  '175px';
-		parentDiv.appendChild(botonDiv);
+		$('#publisherDiv').append(botonDiv);
 	}
-	
+	num_connections += event.connections.length;
 	subscribeToStreams(event.streams);
 }
 
@@ -82,7 +93,13 @@ function subscribeToStreams(streams) {
 		}
 		else {
 			document.getElementById("videoBtn").style.display = 'block';
+		}
+		if (is_ready_for_conversation && num_connections == 2) {
+			is_timer_running = true;
 			startTimer();
+		}
+		else {
+			is_ready_for_conversation = true
 		}
 	}
 }
@@ -93,50 +110,51 @@ function streamCreatedHandler(event) {
 
 function sessionDisconnectedHandler(event) {
 	if (is_reconnect) {
-		is_reconnect = false;
+		num_connections -= 1;
+		showWaitSpiner();
 		publisher = TB.initPublisher(api_key, 'myPublisherDiv', publisherProperties);
 		connect();
 	}
 	else if (is_end_call) {
 		is_end_call = false;
-		alert("The call ended");
+		//$('#waitingDivGone').remove();
+		
 	}
 }
 
 function connectionDestroyedHandler(event) {
+	if (is_reconnect) {
+		showWaitSpiner();
+	}
 }
 
 function signalHandler(event) {
-	//is_reconnect = true;
-	if (is_reconnect) {
-		if ($("#waitingDiv").length == 0) {
-			var waitDiv = document.createElement('div');
-			waitDiv.setAttribute('id','waitingDiv');
-			var waitImg = document.createElement('img');
-			waitImg.setAttribute('src', '/assets/spinner.gif');
-			waitImg.setAttribute('alt', 'Spinner');
-			waitDiv.appendChild(document.createElement('br'));
-			waitDiv.appendChild(document.createTextNode("Waiting for user to connect, please wait."));
-			waitDiv.appendChild(waitImg);
-			$("#waitingDivGone").append(waitDiv);
-		}
-		
-		if (session.connection.connectionId == event.fromConnection.connectionId) {
-			var div = document.createElement('div');
-			div.setAttribute('id','myPublisherDiv');
-			$("#publisherDiv").append(div);
-			is_audio_only = true;
-			document.getElementById("videoBtn").style.display = 'none';
-			session.disconnect();
-		}
-	}
-	else if (is_end_call) {
-		document.getElementById("videoBtn").style.display = 'none';
+	is_reconnect = true;
+	if (session.connection.connectionId == event.fromConnection.connectionId) {
 		is_audio_only = true;
+		document.getElementById("videoBtn").style.display = 'none';
+		var div = document.createElement('div');
+		div.setAttribute('id','myPublisherDiv');
+		$("#publisherDiv").append(div);
 		session.disconnect();
 	}
 }
-	
+
+function showWaitSpiner() {
+	is_reconnect = false;
+	if ($("#waitingDiv").length == 0) {
+		var waitDiv = document.createElement('div');
+		waitDiv.setAttribute('id','waitingDiv');
+		var waitImg = document.createElement('img');
+		waitImg.setAttribute('src', '/assets/spinner.gif');
+		waitImg.setAttribute('alt', 'Spinner');
+		waitDiv.appendChild(document.createElement('br'));
+		waitDiv.appendChild(document.createTextNode("Waiting for user to connect, please wait."));
+		waitDiv.appendChild(waitImg);
+		$("#userDiv").before(waitDiv);
+	}
+}
+
 function enableDisableVideo() {
 	is_audio_only = !is_audio_only
 	publisher.publishVideo(!is_audio_only);
@@ -147,7 +165,7 @@ function enableDisableVideo() {
 }
 
 function startTimer() {
-	if (time_elapsed < total_time) {
+	if (time_elapsed < total_time && is_timer_running) {
 		time_elapsed++;
 		var minutes, seconds;
 		seconds = time_elapsed % 60;
--- app/controllers/interaction_controller.rb
+++ app/controllers/interaction_controller.rb
@@ -5,6 +5,10 @@ class InteractionController < ApplicationController
 		topic = Topic.find(params[:topic_id])
 		next_hint = topic.next_hint(params[:next], params[:current_slide])
 		Pusher[session[6..11]].trigger(session[0..5], {:thumbnail_url => topic.hints[1].thumbnail_url, :current_text => next_hint[:text], :current_image => next_hint[:number]})
+	end
+	
+	def end_call
+		Pusher['channel_messages'].trigger('end_call', {:message => 'call has ended'})
 	end	
 
 end
