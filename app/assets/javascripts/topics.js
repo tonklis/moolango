1d8e81f706b53ab8acf36ee37ccf75fb7190e157
tonklis <benjamin.hernandez@in2teck.com>
2012-08-09 20:35:32 -0400

    More debugging code for heroku

--- app/assets/javascripts/topics.js
+++ app/assets/javascripts/topics.js
@@ -9,7 +9,6 @@
 		var channel_confirm = pusher.subscribe(internal_session);
 
 		channel_confirm.bind('confirm_event', function(data) {
-			alert("RESPUESTA internal session -> " internal_session + " ROOM SESSION " + data.internal_session );
 			redirectToRoom(data);
 		});
 
