0f2d6cae201f6e82be4c7303f69ce79e414de982
tonklis <benjamin.hernandez@in2teck.com>
2012-09-17 17:52:48 -0500

    Added name to text message and changed trackphone number since they are no longer active

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,23 +16,25 @@ class MessagesController < ApplicationController
 	
 		internal_session = params[:internal_session]
 		room = Room.create_available(params[:user_id], params[:topic_id], params[:language_id], internal_session, request.remote_addr)
+    user = User.find(params[:user_id])
 
 		#TestMailer.new_conversation(params[:topic_id], params[:user_id], params[:language_id]).deliver
 
 		@twilio_client = Twilio::REST::Client.new ENV['TWILIO_SID'], ENV['TWILIO_TOKEN']
 		if (Language.find_by_name("english").id == params[:language_id].to_i)
 			number_to_send_to = "6145968264"
-			message = "Waiting to talk about #{Topic.find(params[:topic_id]).name} in #{Language.find(params[:language_id]).name}"
-		else 
-			number_to_send_to = "6142826401"
-			message = "Esperando a hablar sobre #{Topic.find(params[:topic_id]).name} en #{Language.find(params[:language_id]).name}"
+			message = "#{user.firstname} waiting to talk about #{Topic.find(params[:topic_id]).name}"
+		else
+			# change to other phone for Spanish
+			number_to_send_to = "6145968264"
+			message = "#{user.firstname} esperando a hablar sobre #{Topic.find(params[:topic_id]).name}"
 		end
 
-		#@twilio_client.account.sms.messages.create(
-		#	:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
-		#	:to => number_to_send_to,
-		#	:body => message
-		#)
+		@twilio_client.account.sms.messages.create(
+			:from => "+1#{ENV['TWILIO_PHONE_NUMBER']}",
+			:to => number_to_send_to,
+			:body => message
+		)
 
 		respond_to do |format|
 			format.json { render json: {:handshake => false, :room_id => room.id} }
