629850ba70f2ee2499de89366c9a23e2c19969f1
tonklis <benjamin.hernandez@in2teck.com>
2012-12-05 01:22:43 -0600

    Added new format for scheduling and sending emails to the user that just scheduled; TODO: Test it in dev, not ready for production

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -5,27 +5,26 @@ class TestMailer < ActionMailer::Base
 		@topic = Topic.find(topic)
 		@user = User.find(user)
 		@language = Language.find(language.to_i)
-		mail(:to => "tonklis@gmail.com, dmiramon@gmail.com, williambabeaux@gmail.com", :subject => "New conversation with #{@user.firstname}, about #{@topic.name}")
+		mail(:to => "hello@moolango.com", :subject => "New conversation with #{@user.firstname}, about #{@topic.name}")
   end
 
 	def new_purchase(user_id, reference, amount)
 		@user = User.find(user_id)
 		@reference = reference
 		@amount = amount
-		mail(:to => @user.email, :subject => "Order confirmation from MooLango")
+		mail(:to => "#{@user.email}, hello@moolango.com", :subject => "Order confirmation from MooLango")
 	end
 
 	def new_simple_conversation(user_id, room_id)
 		@user = User.find(user_id)
 		@room_id = room_id
-		mail(:to => "tonklis@gmail.com, dmiramon@gmail.com, williambabeaux@gmail.com", :subject => "New conversation with #{@user.firstname}")
+		mail(:to => "hello@moolango.com", :subject => "New conversation with #{@user.firstname}")
   end
 
 	def new_scheduling(schedule)
 		@user = User.find(schedule.user_id)
 		@schedule = schedule
-		mail(:to => "tonklis@gmail.com, dmiramon@gmail.com, williambabeaux@gmail.com", :subject => "New conversation scheduled with #{@user.firstname}")
-		#send mail to the user that is scheduling too
+		mail(:to => "#{@user.email}, hello@moolango.com", :subject => "Your MooLango conversation information")
 	end
 
 end
